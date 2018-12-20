a3b8fcf2922e046fb6eddd476b6af0af06e11df4
oprt <46010229+oprt@users.noreply.github.com>
2018-12-20 11:07:27 +0700

    Update lab 1.sh

--- lab 1.sh
+++ lab 1.sh
@@ -1,3 +1,3 @@
-1. grep -c -i 'Friday' name_file
-2. sed 's/Friday/Saturday/' name_file
+1. grep -c -i 'Friday' crusoe.txt
+2. sed 's/Friday/Saturday/' crusoe.txt
 3. awk '{sum1+=$1} {sum2+=$2} END {print sum1,sum2}' 1.txt
